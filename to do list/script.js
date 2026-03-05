4de6213f277ffc60fc49e230922e8af25b791cae
Gabriel Espínola <gabrielsespinola@gmail.com>
2026-03-05 10:15:02 -0300

    aprimorando a to do list

--- to do list/script.js
+++ to do list/script.js
@@ -1,16 +1,35 @@
 function adicionarTarefa() {
-      let mensagem = "Tarefa adicionada com sucesso!";
-     
-           
+      
+      //recebe valor do input do usuario
+      const inputtarefa  = document.getElementById ("inputtarefa")
+      let tarefa= inputtarefa.value.trim()
+      const mensagem = document.getElementById("mensagem")
+      
+      // se o valor do input estiver vazio então mostre essa mensagem 
+      if(tarefa == ""){
+            let mensagemerro ="ERRO! Digite uma tarefa para adiciona-la a sua lista"
+            mensagem.textContent = mensagemerro;
 
-      let inputtarefa  = document.getElementById ("inputtarefa")
-      let tarefa= inputtarefa.value
-       document.getElementById("mensagem").textContent = mensagem;
-
-       let listatarefas= document.getElementById("listatarefas")
+            mensagem.className="erro";
+            
+      }else {
+      
+      //mensagem de tarefa adicionada com sucesso
+       let mensagemsucesso = "Tarefa adicionada com sucesso!";
+      mensagem.textContent = mensagemsucesso;
+      
+      //Criando um novo item
+       const listatarefas= document.getElementById("listatarefas")
        let novatarefa = document.createElement("li")
       novatarefa.textContent= tarefa
        listatarefas.appendChild(novatarefa)
+
+       mensagem.className=" sucesso"
+      }
       
+      // limpa o input do usuario
        inputtarefa.value= ""
+
+     
+
     }
